8b0574eff40206f44d60dd7f7018e76d37591b03
dhruvhacks <sridhruv@yahoo.com>
2017-10-04 11:22:44 +0530

    Opportunity app initiated

--- portal/settings.py
+++ portal/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'opportunity.apps.OpportunityConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
